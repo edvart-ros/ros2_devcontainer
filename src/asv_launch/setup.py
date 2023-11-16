a6a2f5b547eaed95507498862f39224222ee53ff
edvart-ros <240818@usn.no>
2023-11-16 14:32:39 +0100

    added wamv description

---
+++ src/asv_launch/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+import glob
+
+
+package_name = 'asv_launch'
+share_dir = os.path.join("share", package_name)
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        (share_dir, ["package.xml"]),
+        (os.path.join('share', package_name, 'launch'), ['launch/publish_urdf.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='240818@usn.no',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
